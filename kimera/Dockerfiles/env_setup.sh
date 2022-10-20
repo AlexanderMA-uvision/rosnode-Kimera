5f48449fbed9e05f7dfc612e7ee69fe317f30715
AlexanderMA_uvision <amizrahi@uvision.dk>
2022-10-20 10:36:44 +0200

    init

---
+++ kimera/Dockerfiles/env_setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Creates docker volume with arducam package inside
+KIMERA_PATH=$PWD
+docker volume create --name kimera_volume --opt type=none --opt device="${KIMERA_PATH%/*/*}" --opt o=bind
